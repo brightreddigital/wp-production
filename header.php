be291914345827f1b8bd46aa39ff63ae5b9a3a91
Robert Clarke <robert@brightred.digital>
2024-05-22 16:00:45 +0100

    Update header.php

--- header.php
+++ header.php
@@ -64,7 +64,7 @@
 
 	?>
 
-	<script type="text/javascript" id="cookiebar" src="<?php echo $srcFull ?>"></script>
+	<script type="text/javascript" id="cookie-br" src="<?php echo $srcFull ?>"></script>
 
 <div id="page" class="site">
 
